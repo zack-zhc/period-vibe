3591536ad248b610ef7a0e423500522425605ba9
郑粲 <zack.zhc.work@outlook.com>
2026-05-03 04:35:13 +0800

    修复编译错误：添加缺失的 ColumnScope import

--- app/src/main/java/com/example/periodvibe/ui/settings/components/SettingsComponents.kt
+++ app/src/main/java/com/example/periodvibe/ui/settings/components/SettingsComponents.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
